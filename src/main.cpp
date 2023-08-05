4c791b6687ea2cf93825eed366cf05b20de24160
Djibril Sy <dsy@MacBook-Pro-de-Djibril.local>
2023-08-05 16:23:34 +0200

    Fixed error in main on argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,22 @@
 
 int main(int ac, char **av)
 {
-	unsigned int	serverPort = atoi(av[1]);
-	std::string		serverPassword(av[2]);
-
 	if (ac != 3)
 	{
-		std::cout << "Usage : ./ircserver <port> <password>" << std::endl;
+		std::cout << "Usage : ./ircserver" << BWHITE << " <port> <password>\n" << RESET << std::endl;
 		return 1;
 	}
+
+	unsigned int	serverPort(atoi(av[1]));
+	std::string		serverPassword(av[2]);
 	if ((serverPort == 0 && av[2] != std::string("0")) || serverPassword.empty())
 	{
 		std::cout << "Error: Invalid port or empty password." << std::endl;
 		return 1;
 	}
-
-	// Title bar
 	std::cout << CLEAR << std::endl;
 	std::cout << TITLE << CLEARLINE << " Ft_irc v4.2 - " << IPADDR << " - " << PORT << RESET << std::endl;
 	std::cout << BLUE << "Waiting for connections..." << RESET << std::endl << std::endl;
-	IrcServer server(serverPort, av[2]);
+	IrcServer server(serverPort, serverPassword);
 	server.run();
 }
